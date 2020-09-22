595a9095532230e5a53a1057d11af0c18cc59f90
wangjunjie <wangjunjies@163.com>
2020-09-22 21:12:45 +0800

    新建PageCOntentView

---
+++ SwiftActualCombat/Main/View/PageContentView.swift
@@ -0,0 +1,21 @@
+//
+//  PageContentView.swift
+//  SwiftActualCombat
+//
+//  Created by 王俊杰的Mac mini on 2020/9/22.
+//  Copyright © 2020 王俊杰的Mac mini. All rights reserved.
+//
+
+import UIKit
+
+class PageContentView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
